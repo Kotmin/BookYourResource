2e79b339d65044d56a6325c66cce4b1ff2178a81
kotmin <70173732+Kotmin@users.noreply.github.com>
2024-10-24 02:18:02 +0200

    chore: project init

---
+++ BookYourResource/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookYourResource.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
