4f5d59f95db5cbecb9060772df227f3e356dea1a
kotmin <70173732+Kotmin@users.noreply.github.com>
2024-10-27 22:29:25 +0100

    feat: add reservationrequest model

---
+++ BookYourResource/Models/ReservationRequest.cs
@@ -0,0 +1,10 @@
+namespace MyApp.Models
+{
+    public class ReservationRequest
+    {
+        public string UserId { get; set; }
+        public string ResourceId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
